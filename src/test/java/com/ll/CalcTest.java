a43362f2a02350984c4e5f75b67d6707c4439e5e
fake150907 <yungayeon223@gmail.com>
2023-12-26 17:23:55 +0900

    더하기 기능êµ 기능 구현

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -23,4 +23,17 @@ public class CalcTest {
   void t3() {
     assertThat(Calc.run("2 + 2")).isEqualTo(4);
   }
+
+  @Test
+  @DisplayName("123 + 456 == 579")
+  void t4() {
+    assertThat(Calc.run("123 + 456")).isEqualTo(579);
+  }
+
+  @Test
+  @DisplayName("1000 + 200 == 1200")
+  void t5() {
+    assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
+  }
+
 }
