993e0be41c167a9153917c6dfd85df97597492ff
fake150907 <yungayeon223@gmail.com>
2023-12-27 14:00:34 +0900

    10 + 5 * 2

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,18 +2,40 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
+    boolean needToMultiply = exp.contains("*");
+    boolean needToPlus = exp.contains("+");
+    boolean needToMinus = exp.contains(" - ");
+    boolean needToCompound = needToPlus && needToMultiply;
 
-    exp = exp.replaceAll("\\- ", "\\+ \\-");
 
-    String[] bits = exp.split(" \\+ ");
-    int sum = 0;
-
-    for(int i = 0; i < bits.length; i++){
-      int a = Integer.parseInt(bits[i]);
-      sum += a;
+    if (needToCompound) {
+      String[] bits = exp.split(" \\+ ");
+      return Integer.parseInt(bits[0]) + run(bits[1]);
     }
 
-    return sum;
+    if (needToPlus || needToMinus) {
+      exp = exp.replaceAll("\\- ", "\\+ \\-");
 
+      String[] bits = exp.split(" \\+ ");
+
+      int sum = 0;
+
+      for (int i = 0; i < bits.length; i++) {
+        sum += Integer.parseInt(bits[i]);
+      }
+
+      return sum;
+    } else if (needToMultiply) {
+      String[] bits = exp.split(" \\* ");
+
+      int rs = 1;
+
+      for (int i = 0; i < bits.length; i++) {
+        rs *= Integer.parseInt(bits[i]);
+      }
+      return rs;
+    }
+
+    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
